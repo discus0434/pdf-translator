a6d244039cec90e1682afb85c5bd43cf9260d7fb
EC2 Default User <ec2-user@ip-172-31-6-183.ap-northeast-1.compute.internal>
2023-04-20 04:09:31 +0000

    folder_mode

--- translator.py
+++ translator.py
@@ -1,18 +1,13 @@
 import argparse
+import requests
 from pathlib import Path
-import subprocess
-
-# if import requests fails, try pip install requests
-try:
-    import requests
-except Exception:
-    subprocess.check_call(["python3", "-m", "pip", "install", "requests"])
-    import requests
+import glob
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--input_pdf_path", type=Path, required=True)
+    parser.add_argument("-i", "--input_pdf_path", type=Path)
     parser.add_argument("-o", "--output_dir", type=Path, default="./outputs")
+    parser.add_argument("-f", "--folder_mode", action="store_true")
     args = parser.parse_args()
 
     translate_url = "http://localhost:8765/translate_pdf/"
@@ -20,13 +15,26 @@ if __name__ == "__main__":
 
     args.output_dir.mkdir(parents=True, exist_ok=True)
 
-    with open(args.input_pdf_path, "rb") as input_pdf:
-        response = requests.post(translate_url, files={"input_pdf": input_pdf})
+    def translate_pdf(input_path, output_dir):
+        with open(input_path, "rb") as input_pdf:
+            response = requests.post(translate_url, files={"input_pdf": input_pdf})
 
-    if response.status_code == 200:
-        with open(args.output_dir / args.input_pdf_path.name, "wb") as output_pdf:
-            output_pdf.write(response.content)
-        print(f"Converted PDF saved to {args.output_dir / args.input_pdf_path.name}")
-        requests.get(clear_temp_url)
+        if response.status_code == 200:
+            with open(output_dir / input_path.name, "wb") as output_pdf:
+                output_pdf.write(response.content)
+            print(f"Converted PDF saved to {output_dir / input_path.name}")
+            requests.get(clear_temp_url)
+        else:
+            print(f"An error occurred: {response.status_code}")
+
+    if args.folder_mode:
+        input_dir = Path("./inputs")
+        pdf_files = glob.glob(f"{input_dir}/*.pdf")
+
+        for pdf_file in pdf_files:
+            input_path = Path(pdf_file)
+            translate_pdf(input_path, args.output_dir)
+    elif args.input_pdf_path:
+        translate_pdf(args.input_pdf_path, args.output_dir)
     else:
-        print(f"An error occurred: {response.status_code}")
+        print("Please provide either --input_pdf_path or --folder_mode options.")
