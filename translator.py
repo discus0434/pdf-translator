a399d04c0a374526b4f7904ef8fe0d432d679e2c
discus0434 <discus0434@gmail.com>
2023-04-20 15:12:25 +0900

    refactor translator.py

--- translator.py
+++ translator.py
@@ -23,11 +23,6 @@ def translate_request(input_pdf_path: Path, output_dir: Path) -> None:
         Path to the PDF to be translated.
     output_dir : Path
         Path to the directory where the translated PDF will be saved.
-
-    Raises
-    ------
-    ValueError
-        If the input path is not a valid path to file or directory.
     """
     print(f"Translating {input_pdf_path}...")
     with open(input_pdf_path, "rb") as input_pdf:
@@ -42,12 +37,28 @@ def translate_request(input_pdf_path: Path, output_dir: Path) -> None:
         print(f"An error occurred: {response.status_code}")
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--input_pdf_path_or_dir", type=Path, required=True)
-    parser.add_argument("-o", "--output_dir", type=Path, default="./outputs")
-    args = parser.parse_args()
+def main(args: argparse.Namespace) -> None:
+    """Translates a PDF or all PDFs in a directory.
 
+    Parameters
+    ----------
+    args : argparse.Namespace
+        Arguments passed to the script.
+
+    Raises
+    ------
+     ValueError
+        If the input path is not a valid path to file or directory.
+
+    Notes
+    -----
+    args must have the following attributes:
+        input_pdf_path_or_dir : Path
+            Path to the PDF or directory of PDFs to be translated.
+        output_dir : Path
+            Path to the directory where the translated PDFs
+            will be saved.
+    """
     args.output_dir.mkdir(parents=True, exist_ok=True)
 
     if args.input_pdf_path_or_dir.is_file():
@@ -71,3 +82,23 @@ if __name__ == "__main__":
         )
 
     print("Done.")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-i",
+        "--input_pdf_path_or_dir",
+        type=Path,
+        required=True,
+        help="Path to the PDF or directory of PDFs to be translated.",
+    )
+    parser.add_argument(
+        "-o",
+        "--output_dir",
+        type=Path,
+        default="./outputs",
+        help="Path to the directory where the translated PDFs will be saved. (default: ./outputs)",
+    )
+    args = parser.parse_args()
+    main(args)
